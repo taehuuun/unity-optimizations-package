48e534365a1b576f2999a75bdcfac3939f2e13bd
LimTaeHun <lsw463@naver.com>
2023-07-24 19:42:01 +0900

    Update: Translate text in TextureOptimization.cs to Korean

--- Assets/CrazyOptimizer/Editor/WindowComponents/TextureOptimizations/TextureOptimization.cs
+++ Assets/CrazyOptimizer/Editor/WindowComponents/TextureOptimizations/TextureOptimization.cs
@@ -24,8 +24,8 @@ namespace CrazyGames.WindowComponents.TextureOptimizations
             EditorGUILayout.BeginHorizontal();
             GUILayout.FlexibleSpace();
             EditorGUILayout.BeginVertical();
-            GUILayout.Label("Press \"Analyze textures\" button to load the table.");
-            GUILayout.Label("Press it again when you need to refresh the data.");
+            GUILayout.Label("\"텍스처 분석\" 버튼을 눌러 테이블을 로드하세요.");
+            GUILayout.Label("데이터를 새로고침하려면 다시 누르세요.");
             EditorGUILayout.EndVertical();
             GUILayout.FlexibleSpace();
             EditorGUILayout.EndHorizontal();
@@ -36,14 +36,14 @@ namespace CrazyGames.WindowComponents.TextureOptimizations
             EditorGUILayout.Space(5);
             EditorGUILayout.BeginHorizontal();
             GUILayout.FlexibleSpace();
-            if (GUILayout.Button(_isAnalyzing ? "Analyzing..." : "Analyze textures", GUILayout.Width(200)))
+            if (GUILayout.Button(_isAnalyzing ? "분석 중..." : "텍스처 분석", GUILayout.Width(200)))
             {
                 AnalyzeTextures();
             }
 
             var originalValue = EditorGUIUtility.labelWidth;
             EditorGUIUtility.labelWidth = 160;
-            _includeFilesFromPackages = EditorGUILayout.Toggle("Include files from Packages", _includeFilesFromPackages);
+            _includeFilesFromPackages = EditorGUILayout.Toggle("패키지의 파일 포함", _includeFilesFromPackages);
             EditorGUIUtility.labelWidth = originalValue;
             GUILayout.FlexibleSpace();
 
@@ -51,16 +51,16 @@ namespace CrazyGames.WindowComponents.TextureOptimizations
             EditorGUILayout.Space(5);
 
             GUILayout.Label(
-                "This utility gives you an overview of the textures used in your project. By optimizing various settings, you will be able to considerably decrease your final build size. You can click on a texture to select it in the Project view. To find out more about how the tool finds the textures, please check our GitHub repo.",
+                "이 유틸리티는 프로젝트에서 사용되는 텍스처의 개요를 제공합니다. 여러 설정을 최적화함으로써 최종 빌드 크기를 상당히 줄일 수 있습니다. 프로젝트 뷰에서 텍스처를 선택하려면 클릭하세요. 도구가 텍스처를 찾는 방법에 대해 자세히 알아보려면 GitHub 저장소를 확인하세요.",
                 EditorStyles.wordWrappedLabel);
 
 
-            BuildExplanation("Max size",
-                "Decrease the max size as much as possible while the texture still looks good in game. You most likely don't need the default 2048 set by Unity.");
-            BuildExplanation("Compression", "Lower quality will decrease the final build size.");
-            BuildExplanation("Crunch compression",
-                "All the textures with crunch compression enabled will be compressed together, decreasing the final build size.");
-            BuildExplanation("Crunch comp. quality", "A higher compression quality means larger textures and longer compression times.");
+            BuildExplanation("최대 크기",
+                "텍스처가 게임에서 여전히 좋아 보이는 한 최대한 크기를 줄입니다. Unity에서 설정한 기본값 2048은 아마 필요하지 않을 것입니다.");
+            BuildExplanation("압축", "품질을 낮추면 최종 빌드 크기가 줄어듭니다.");
+            BuildExplanation("크런치 압축",
+                "크런치 압축이 활성화된 모든 텍스처는 함께 압축되어 최종 빌드 크기를 줄입니다.");
+            BuildExplanation("크런치 압축 품질", "압축 품질이 높을수록 텍스처 크기가 크고 압축 시간이 더 길어집니다.");
         }
 
         static void BuildExplanation(string label, string explanation)
@@ -180,14 +180,14 @@ namespace CrazyGames.WindowComponents.TextureOptimizations
                 _multiColumnHeaderState = new MultiColumnHeaderState(new[]
                 {
                     // when adding a new column don't forget to check the sorting method, and the CellGUI method
-                    new MultiColumnHeaderState.Column() {headerContent = new GUIContent() {text = "Texture"}, width = 150, minWidth = 150, canSort = true},
-                    new MultiColumnHeaderState.Column() {headerContent = new GUIContent() {text = "Type"}, width = 60, minWidth = 60, canSort = true},
-                    new MultiColumnHeaderState.Column() {headerContent = new GUIContent() {text = "Max size"}, width = 60, minWidth = 60, canSort = true},
-                    new MultiColumnHeaderState.Column() {headerContent = new GUIContent() {text = "Compression"}, width = 80, minWidth = 80, canSort = true},
+                    new MultiColumnHeaderState.Column() {headerContent = new GUIContent() {text = "텍스처"}, width = 150, minWidth = 150, canSort = true},
+                    new MultiColumnHeaderState.Column() {headerContent = new GUIContent() {text = "타입"}, width = 60, minWidth = 60, canSort = true},
+                    new MultiColumnHeaderState.Column() {headerContent = new GUIContent() {text = "최대 크기"}, width = 60, minWidth = 60, canSort = true},
+                    new MultiColumnHeaderState.Column() {headerContent = new GUIContent() {text = "압축"}, width = 80, minWidth = 80, canSort = true},
                     new MultiColumnHeaderState.Column()
-                        {headerContent = new GUIContent() {text = "Crunch compression"}, width = 120, minWidth = 120, canSort = true},
+                        {headerContent = new GUIContent() {text = "크런치 압축"}, width = 120, minWidth = 120, canSort = true},
                     new MultiColumnHeaderState.Column()
-                        {headerContent = new GUIContent() {text = "Crunch comp. quality"}, width = 128, minWidth = 128, canSort = true},
+                        {headerContent = new GUIContent() {text = "크런치 압축 품질"}, width = 128, minWidth = 128, canSort = true},
                 });
             _textureCompressionTree = new TextureTree(treeViewState, new MultiColumnHeader(_multiColumnHeaderState), treeModel);
             _isAnalyzing = false;
